accd61c280fc925755f1110a501cfb3393fca234
nirmalravidas <nirmalravidas.email@gmail.com>
2026-05-17 13:25:44 +0530

    add lead and user enums

---
+++ backend/src/enums/lead-enum.ts
@@ -0,0 +1,17 @@
+export enum LeadStatus {
+    NEW = 'New',
+    CONTACTED = 'Contacted',
+    QUALIFIED = 'Qualified',
+    LOST = 'Lost',
+}
+
+export enum LeadSource {
+    WEBSITE = 'Website',
+    INSTAGRAM = 'Instagram',
+    REFERRAL = 'Referral', 
+}
+
+export enum SortOrder {
+    LATEST = 'latest',
+    OLDEST = 'oldest',
+}
