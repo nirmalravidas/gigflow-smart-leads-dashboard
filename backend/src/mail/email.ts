57d7f0345156a62910f7c541d4c4b3655987403b
nirmalravidas <nirmalravidas.email@gmail.com>
2026-05-17 12:54:06 +0530

    feat: implement email sending services for welcome, verification, and password reset flows

--- backend/src/mail/email.ts
+++ backend/src/mail/email.ts
@@ -1,1 +1,63 @@
 import {transporter, sender} from './nodemailer';
+import { generatePasswordResetEmail, generatePasswordResetSuccessEmail, generateVerificationCodeEmail, generateWelcomeEmail } from './emailTemplate';
+
+
+export const sendWelcomeEmail = async (email: string, name: string) => {
+    const htmlContent = generateWelcomeEmail(name);
+
+    try {
+        await transporter.sendMail({
+            from: `${sender.name} <${sender.email}>`,
+            to: email,
+            subject: "Welcome",
+            html: htmlContent,
+        });
+    } catch (error) {
+        throw new Error("Failed to send welcome email");
+    }
+};
+
+export const sendPasswordResetEmail = async (email: string, resetUrl: string) => {
+  const htmlContent = generatePasswordResetEmail(resetUrl);
+  try {
+    await transporter.sendMail({
+      from: `${sender.name} <${sender.email}>`,
+      to: email,
+      subject: "Reset Password",
+      html: htmlContent,
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("failed to send password reset email");
+  }
+};
+
+export const sendPasswordResetSuccessEmail = async (email: string) => {
+  const htmlContent = generatePasswordResetSuccessEmail();
+  try {
+    await transporter.sendMail({
+      from: `${sender.name} <${sender.email}>`,
+      to: email,
+      subject: "Password reset successfully.",
+      html: htmlContent,
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to send password reset successful email");
+  }
+};
+
+export const sendVerificationCodeEmail = async (email: string, verificationToken: string) => {
+  const htmlContent = generateVerificationCodeEmail(verificationToken);
+  try {
+    await transporter.sendMail({
+      from: `${sender.name} <${sender.email}>`,
+      to: email,
+      subject: "verify your email.",
+      html: htmlContent,
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to send email verification email.");
+  }
+};
