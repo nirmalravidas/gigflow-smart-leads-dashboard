c0f55cc852448b0b903e23de9fd09fff346fdafe
nirmalravidas <nirmalravidas.email@gmail.com>
2026-05-17 13:36:43 +0530

    add user interface

---
+++ backend/src/interfaces/user-interface.ts
@@ -0,0 +1,31 @@
+import { UserRole } from "../enums/userRole-enum";
+import { Types } from "mongoose";
+
+export interface IUser {
+    _id: Types.ObjectId;
+    name: string;
+    email: string;
+    password: string;
+    role: UserRole;
+    isEmailverified: boolean;
+    emailverificationToken?: string;
+    emailVerificationExpires?: Date;
+    passwordResetToken?:string;
+    passwordResetExpires?: Date;
+    refreshToken?: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface IUserPublic {
+    _id: Types.ObjectId;
+    name: string;
+    email: string;
+    role: UserRole;
+    isEmailVerified: boolean;
+    createdAt: Date;
+}
+
+export interface IUserMethods {
+    comparePassword(candidatePassword: string): Promise<boolean>;
+}
